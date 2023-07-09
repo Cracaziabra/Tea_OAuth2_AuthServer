aa15b9d344583675beed602d79268fd3b0711f27
Cracaziabra <Pashamkh19@gmail.com>
2023-07-09 16:34:02 +0700

    Test CI. Cleared imports in CustomClientServiceTest.java

--- src/test/java/com/example/oauth2_authserver/services/CustomClientServiceTest.java
+++ src/test/java/com/example/oauth2_authserver/services/CustomClientServiceTest.java
@@ -4,24 +4,15 @@ import com.example.oauth2_authserver.entities.Client;
 import com.example.oauth2_authserver.entities.GrantType;
 import com.example.oauth2_authserver.entities.RedirectUri;
 import com.example.oauth2_authserver.repositories.ClientRepository;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.util.HashSet;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
